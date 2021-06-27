06fc87df5d1e9b059980a4ef37ae587f4cfb856c
Furkan Karataş <karatas.furkan@outlook.com>
2021-06-27 16:20:57 +0300

    created dal classses and dbcontext

---
+++ DataAccess/Concrete/EntityFramework/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class DataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=databaseName;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
+
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<ItData> ItDatas { get; set; }
+        public DbSet<TrData> TrDatas { get; set; }
+    }
+}
